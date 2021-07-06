80dbb3af446e4dd5c18b99cbadff71b567838288
Kevin <kevin.morales05@epn.edu.ec>
2021-07-06 18:42:19 -0500

    #6 Rutas y pruebas en Postman

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Use App\Models\Article;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/articles', function() {
+ return Article::all();
+});
+Route::get('/articles/{id}', function($id) {
+ return Article::find($id);
+});
+Route::post('/articles', function(Request $request) {
+ return Article::create($request->all());
+});
+Route::put('/articles/{id}', function(Request $request, $id) {
+ $article = Article::findOrFail($id);
+ $article->update($request->all());
+ return $article;
+});
+Route::delete('/articles/{id}', function($id) {
+ Article::find($id)->delete();
+ return 204;
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
