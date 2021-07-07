d30bf90e504efbd93664f5748746adf85ec475c0
Kevin <kevin.morales05@epn.edu.ec>
2021-07-06 19:08:55 -0500

    #7 Controladores y CORS

--- routes/api.php
+++ routes/api.php
@@ -4,24 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Use App\Models\Article;
 
-Route::get('/articles', function() {
- return Article::all();
-});
-Route::get('/articles/{id}', function($id) {
- return Article::find($id);
-});
-Route::post('/articles', function(Request $request) {
- return Article::create($request->all());
-});
-Route::put('/articles/{id}', function(Request $request, $id) {
- $article = Article::findOrFail($id);
- $article->update($request->all());
- return $article;
-});
-Route::delete('/articles/{id}', function($id) {
- Article::find($id)->delete();
- return 204;
-});
+use App\Http\Controllers\ArticleController;
+
+Route::get('/articles', [ArticleController::class,'index']);
+Route::get('/articles/{article}', [ArticleController::class,'show']);
+Route::post('/articles', [ArticleController::class,'store']);
+Route::put('/articles/{article}', [ArticleController::class,'update']);
+Route::delete('/articles/{article}', [ArticleController::class,'delete']);
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
