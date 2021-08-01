6933f34ae1dba5eb3cb0e128e4cf18f987e35635
kevinmorales05 <kevin.morales05@epn.edu.ec>
2021-08-01 12:32:16 -0500

    agrego css especifico

--- resources/views/emails/comments/new.blade.php
+++ resources/views/emails/comments/new.blade.php
@@ -13,13 +13,15 @@ Ver Comentario
 
 @component('mail::panel')
 This is the panel content.
+
 @endcomponent
 @component('mail::table')
 |Laravel      | Table         | Example  |
-|-------------|:-------------:| --------:|
+|:------------|:-------------:| --------:|
 | Col 2 is    | Centered      | $10      |
 | Col 3 is    | Right-Aligned | $20      |
 @endcomponent
+
 
 @component('mail::subcopy')
  This is the subcopy content.
